4e32bedb2799d856fae21c55dff5258ef36d543a
ub2204 <jiaguo@ub2204.com>
2024-09-08 15:38:09 +0800

    Implementing the Recipes REST handlers

---
+++ go-rest-demo/amystarter/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"net/http"
+	"regexp"
+
+	"github.com/xNok/go-rest-demo/pkg/recipes"
+)
+
+var (
+	RecipeRe       = regexp.MustCompile(`^/recipes/*$`)
+	RecipeReWithID = regexp.MustCompile(`^/recipes/([a-z0-9]+(?:-[a-z0-9]+)+)$`)
+)
+
+func main() {
+	// Create the Store and Recipe Handler
+	store := recipes.NewMemStore()
+	recipesHandler := NewRecipesHandler(store)
+
+	// Create a new request multiplexer
+	// Take incoming requests and dispatch them to the matching handlers
+	mux := http.NewServeMux()
+
+	// Register the routes and handlers
+	mux.Handle("/", &homeHandler{})
+	mux.Handle("/recipes", recipesHandler)
+	mux.Handle("/recipes/", recipesHandler)
+	// Run the server
+	http.ListenAndServe(":8080", mux)
+}
+
+type homeHandler struct{}
+
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is my home page"))
+}
+
+// recipeStore represent a data storage containing recipes
+type recipeStore interface {
+	Add(name string, recipe recipes.Recipe) error
+	Get(name string) (recipes.Recipe, error)
+	List() (map[string]recipes.Recipe, error)
+	Update(name string, recipe recipes.Recipe) error
+	Remove(name string) error
+}
+
+// RecipesHandler implements http.Handler and dispatches requests to the store
+type RecipesHandler struct {
+	store recipeStore
+}
+
+func NewRecipesHandler(s recipeStore) *RecipesHandler {
+	return &RecipesHandler{
+		store: s,
+	}
+}
+
+func (h *RecipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.Method == http.MethodPost && RecipeRe.MatchString(r.URL.Path):
+		h.CreateRecipe(w, r)
+		return
+	case r.Method == http.MethodGet && RecipeRe.MatchString(r.URL.Path):
+		h.ListRecipes(w, r)
+		return
+	case r.Method == http.MethodGet && RecipeReWithID.MatchString(r.URL.Path):
+		h.GetRecipe(w, r)
+		return
+	case r.Method == http.MethodPut && RecipeReWithID.MatchString(r.URL.Path):
+		h.UpdateRecipe(w, r)
+		return
+	case r.Method == http.MethodDelete && RecipeReWithID.MatchString(r.URL.Path):
+		h.DeleteRecipe(w, r)
+		return
+	default:
+		return
+	}
+}
+
+func (h *RecipesHandler) CreateRecipe(w http.ResponseWriter, r *http.Request) {}
+func (h *RecipesHandler) ListRecipes(w http.ResponseWriter, r *http.Request)  {}
+func (h *RecipesHandler) GetRecipe(w http.ResponseWriter, r *http.Request)    {}
+func (h *RecipesHandler) UpdateRecipe(w http.ResponseWriter, r *http.Request) {}
+func (h *RecipesHandler) DeleteRecipe(w http.ResponseWriter, r *http.Request) {}
